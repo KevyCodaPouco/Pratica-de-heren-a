f4358042366a4cf96350d9fbf3acb055ff7a96ec
10724113971 <10724113971@UNS11DT098864.animaedu.intranet>
2024-04-09 20:59:29 -0300

    projeto feito em POO com a finalidade de desenvolver a herença de clases

---
+++ src/main/java/model/Restaurante.java
@@ -0,0 +1,56 @@
+package model;
+
+import javax.swing.JOptionPane;
+
+public final class Restaurante extends Empresa {
+
+    private String tipoComida;
+    private double preçoMedio;
+
+    public Restaurante() {
+        this("", "", "", "", "", "", "", 0);
+    }
+
+    public Restaurante(String nome, String endereco, String cidade,
+            String estado, String cep, String fone,
+            String tipoComida, double precoMedio) {
+        super(nome, endereco, cidade, estado, cep, fone);
+        setTipoComida(tipoComida);
+        setPreçoMedio(precoMedio);
+    }
+
+    public String getTipoComida() {
+        return tipoComida;
+    }
+
+    public void setTipoComida(String tipoComida) {
+        this.tipoComida = tipoComida;
+    }
+
+    public double getPreçoMedio() {
+        return preçoMedio;
+    }
+
+    public void setPreçoMedio(double preçoMedio) {
+        this.preçoMedio = preçoMedio;
+    }
+    @Override
+    public void leitura(){
+        super.leitura();
+        setTipoComida(JOptionPane.showInputDialog("Tipo de Comida:"));
+        setPreçoMedio(Double.parseDouble(JOptionPane.showInputDialog("")));
+    }
+    @Override
+    public void imprimir(){
+        JOptionPane.showMessageDialog(null, paraString());   
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String paraString(){
+        return(super.paraString() + "\ntipo de comida " + getTipoComida() + "\npreço medio " + getPreçoMedio());
+    }
+}
---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,26 @@
+package principal;
+
+import javax.swing.JOptionPane;
+import model.Restaurante;
+
+public class Principal {
+
+    public static void main(String args[]) {
+        Restaurante restaurante = new Restaurante();
+        int op = -1;
+        while (op != 0) {
+            op = Integer.parseInt(JOptionPane.showInputDialog(
+                    "1 - Preencher Restaurante"
+                    + "\n2 - Imprimir Restaurante"
+                    + "\n0 - Sair"));
+            switch (op) {
+                case 1:
+                    restaurante.leitura();
+                    break;
+                case 2:
+                    restaurante.imprimir();
+                    break;
+            }
+        }
+    }
+}
